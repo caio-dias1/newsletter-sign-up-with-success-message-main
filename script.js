84acb2c84d6ba8ee6c5b209d140898fc4cc99993
Caio Dias <caior.dias11@gmail.com>
2024-04-28 17:42:18 -0300

    mobile-version-full+validation-test

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function submit () {
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.getElementById('email').value;
+    if (validateEmail(email)) {
+        window.alert('email é valido');
+    } else {
+        window.alert('digite um email valido');
+    }
+});
 
+function validateEmail(email) {
+    var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email)
 }
