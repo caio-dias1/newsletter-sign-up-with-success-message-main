8cc33c6ede366abaa4487a87010a0c7e560e1c24
Caio Dias <caior.dias11@gmail.com>
2024-04-30 21:24:03 -0300

    validation+final-mobile-version

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 document.getElementById('emailForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    var email = document.getElementById('email').value;
+    var email = document.getElementById('email').value
+    var img = document.getElementById('img')
+    var form = document.getElementById('form')
+    var confirm = document.getElementById('confirm')
     if (validateEmail(email)) {
-        window.alert('email é valido');
+        img.style.display = 'none'
+        form.style.display = 'none'
+        confirm.style.display = 'flex'
     } else {
-        window.alert('digite um email valido');
+        window.alert('Digite um email valido!')
     }
 });
+
+function confirmar() {
+    var confirm = document.getElementById('confirm')
+    confirm.style.display = 'none'
+    img.style.display = ''
+    form.style.display = ''
+}
 
 function validateEmail(email) {
     var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
